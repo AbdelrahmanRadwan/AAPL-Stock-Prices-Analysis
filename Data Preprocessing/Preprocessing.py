dba2499ab8090a8daec99b17e59926b76cebe44d
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-03-25 21:25:14 +0200

    Added the percentage of difference

--- Data Preprocessing/Preprocessing.py
+++ Data Preprocessing/Preprocessing.py
@@ -9,16 +9,28 @@ csv_file_i = "/home/radwan/Desktop/AAPL Stock Prices Analysis/Meta Data/AAPL.csv
 
 #load the input in a data frame
 df = pd.read_csv(csv_file_i)
+
 #new column - the days of the week, it includes the names of the days
 day_of_week = list()
+
+#new column - percentage of difference between the	Open and Close on the same day
+same_day_delta = list()
 
 #walk through all the Dates to extract the day's name
 for date in df["Date"]:
     day_of_week.append(parser.parse(date).strftime("%a"))
 
-#add a new column in the data frame with the day name
-df["day_of_week"]=day_of_week
+#walk through all the days to extract the same day delta
+#percentage difference = diff/avg * 100%
+for i in range(len(df)):
+    diff = df["Open"][i] - df["Close"][i]
+    avg = (df["Open"][i] + df["Close"][i])/2
+    same_day_delta.append(diff/avg *100)
 
+#add a new column in the data frame with the day name
+df["day_of_week"] = day_of_week
+#add a new column in the data frame with the percentage of difference
+df["same_day_delta"] = same_day_delta
 
 #Write DateFrame back as csv file
 df.to_csv(csv_file_o, index=False)
