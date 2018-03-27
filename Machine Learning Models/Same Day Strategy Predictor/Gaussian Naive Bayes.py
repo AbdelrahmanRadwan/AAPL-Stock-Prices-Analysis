d1e7abf39ba3b4373553edfa895fe8b46ede30fd
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-03-27 10:15:53 +0200

    Added the learning part in the machine learning model

--- Machine Learning Models/Same Day Strategy Predictor/Gaussian Naive Bayes.py
+++ Machine Learning Models/Same Day Strategy Predictor/Gaussian Naive Bayes.py
@@ -1,9 +1,8 @@
-import sklearn
-from sklearn.datasets import load_breast_cancer
-from dateutil import parser
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import math
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
 csv_file_i = "../../DataSet/Dataset.csv"
 df = pd.read_csv(csv_file_i)
 
@@ -15,9 +14,17 @@ labels = list(df['same_day_strategy'])
 #The features (Input)...
 features = list(df['same_day_delta'])
 
+
+
 # Split our data to be 33% testing - the rest for training
 train, test, train_labels, test_labels = train_test_split(features,
                                                         labels,
                                                         test_size=0.33,
                                                         random_state=42)
+# Initialize our classifier
+gnb = GaussianNB()
+
+# Train our classifier
+model = gnb.fit(train, train_labels)
+
 
