df8f6214dedaa8981f93a8c9347674fef52bd8e3
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-03-28 01:58:38 +0200

    Added the tester for the Naive Bayes classifer with 0.963942307692 accuracy

--- Machine Learning Models/Same Day Strategy Predictor/Gaussian Naive Bayes.py
+++ Machine Learning Models/Same Day Strategy Predictor/Gaussian Naive Bayes.py
@@ -2,18 +2,20 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 
 csv_file_i = "../../DataSet/Dataset.csv"
 df = pd.read_csv(csv_file_i)
 
-
 label_names = [0, 1]
 feature_names = ["same_day_delta"]
 #The labels (targets)...
-labels = list(df['same_day_strategy'])
+labels = np.array(df['same_day_strategy'])
 #The features (Input)...
-features = list(df['same_day_delta'])
-
+features = np.array(df['same_day_delta'])
+# Re-shape the features vector so we can do multiplications
+features = features.reshape((len(features),1))
 
 
 # Split our data to be 33% testing - the rest for training
@@ -21,10 +23,13 @@ train, test, train_labels, test_labels = train_test_split(features,
                                                         labels,
                                                         test_size=0.33,
                                                         random_state=42)
+# Building the Model
 # Initialize our classifier
 gnb = GaussianNB()
-
 # Train our classifier
 model = gnb.fit(train, train_labels)
 
+# Evaluating the Model
+preds = gnb.predict(test)
 
+print(accuracy_score(test_labels, preds))
